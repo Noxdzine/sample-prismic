9ca98c75be29cb95f581023ffb50ca9ca639ef2f
Anthony AUBERTIN <anthony.aubertin@gmail.com>
2016-07-27 16:39:18 +0200

    first commit

---
+++ app/conf/prismic-configuration.js
@@ -0,0 +1,11 @@
+export default {
+
+  apiEndpoint: 'https://jskit-demo.prismic.io/api'
+
+  // -- Access token if the Master is not open
+  // accessToken: 'xxxxxx',
+
+  // OAuth
+  // clientId: 'xxxxxx',
+  // clientSecret: 'xxxxxx',
+}
